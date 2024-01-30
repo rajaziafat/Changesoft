1a22641be6b7c718b33eba8710e1eef8e404c338
shumailrafique-info <shumailrafique.info@gmail.com>
2024-01-30 20:27:13 +0500

    changes to buttons and font

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
         <header className="flex w-full bg-[#D3E3FB]/35 h-[80px] sm:h-[100px] items-center justify-between pl-[10px] sm:pl-[20px] md:pl-[76px] pr-[10px] sm:pr-[20px] md:pr-[47px]">
             {/* Left  */}
             <Link to={"/"}>
-                <p className="sm:text-[14px] md:text-[16px] font-[600]">CHAINSOFT</p>
+                <p className="text-[18px] sm:text-[22px] md:text-[22px] font-[700]">CHAINSOFT</p>
             </Link>
             {/* Right */}
             <div className="flex items-center justify-center gap-[10px] sm:gap-[15px] md:gap-[34px] font-[400]">
-                <button className="md:pt-[7.2px] pt-[5px] pb-[7px] md:pb-[9.2px]  leading-[1] text-[14px] md:text-[16px] w-[80px] sm:w-[100px] md:w-[118px] bg-[white] border-[2px] border-[#4770EA] rounded-[16px] center text-[#4972EB]">Login</button>
-                <button className="md:pt-[7.2px] pt-[5px] pb-[7px] md:pb-[9.2px]  leading-[1] text-[14px] md:text-[16px] w-[80px] sm:w-[100px] md:w-[118px] text-[white] bg-[#4770EA] border-[1px] border-[#4770EA] rounded-[16px] center">Sign up</button>
+                <button  style={{transition:".5s ease all"}} className="md:pt-[7.2px] pt-[5px] pb-[7px] md:pb-[9.2px]  leading-[1] text-[14px] md:text-[16px] w-[80px] sm:w-[100px] md:w-[118px] bg-[white] hover:bg-[#4770EA] hover:text-[white] border-[2px] border-[#4770EA] rounded-[16px] center text-[#4972EB]">Login</button>
+                <button  style={{transition:".5s ease all"}} className="md:pt-[7.2px] pt-[5px] pb-[7px] md:pb-[9.2px]  leading-[1] text-[14px] md:text-[16px] w-[80px] sm:w-[100px] md:w-[118px] text-[white] hover:text-[#4770EA] hover:bg-white bg-[#4770EA] border-[2px] border-[#4770EA] rounded-[16px] center">Sign up</button>
             </div>
         </header>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,21 @@ import { Link } from "react-router-dom"
 const Home = () => {
     return (
         <div className=" lg:h-[calc(100dvh-100px)] flex flex-col-reverse gap-[40px] lg:gap-0 items-center py-[50px] lg:py-0 justify-center lg:grid lg:grid-cols-2 px-[20px] lg:px-0">
-            <div className="flex items-center justify-start lg:pl-[40px]">
+            <div className="flex items-center justify-start lg:pl-[50px] 2xl:pl-[80px]">
                 <div className="flex-col gap-[25px] items-center justify-center flex lg:block">
-                    <p className="lg:text-start text-center">
-                        Welcome to Chainsoft – Your Gateway to Seamless Blockchain implementation and Integration!
+                    <p className="lg:text-start text-[26px] sm:text-[35px] xl:text-[40px] 2xl:text-[50px] max-w-[700px] lg:max-w-auto font-[700] text-center leading-[1.3] text-[#000000e3]">
+                        Welcome to Chainsoft – <br className="lg:flex hidden" /> Your Gateway to Seamless <br className="lg:flex hidden" /> Blockchain implementation <br className="lg:flex hidden" /> and Integration!
                     </p>
-                    <div className="w-[227px] h-[3px] bg-[#216EFC] lg:mt-[23px]">
+                    <div className="w-[270px] sm:w-[327px] h-[3px] bg-[#216EFC] mt-[10px] lg:mt-[40px]">
 
                     </div>
                     <Link to={"/about"}>
-                        <button className="grade bg-gradient-to-r from-[#FFFFFF] rounded-full to-[#216EFC] py-[5px] sm:py-0 sm:h-[48px] w-[130px] sm:w-[190px] text-[white] flex items-center justify-center text-[20px] sm:text-[26px] lg:mt-[140px]">Boshlash</button>
+                        <button style={{transition:".5s ease all"}} className="grade font-[400] rounded-full bg-[#216EFC] hover:text-[#216EFC] hover:bg-white py-[5px] sm:py-0 sm:h-[48px] w-[130px] sm:w-[190px] text-[white] flex items-center justify-center text-[20px] sm:text-[22px] mt-[10px] sm:mt-[40px]">Boshlash</button>
                     </Link>
                 </div>
             </div>
             <div className="flex items-center justify-center w-full h-full">
-                <img src="./hero.png" className="w-full 2xl:max-w-[700px] max-w-[400px] lg:max-w-[560px]" alt="" />
+                <img src="./hero.png" className="w-full 2xl:max-w-[600px] max-w-[400px] lg:max-w-[450px]" alt="" />
             </div>
         </div>
     )
